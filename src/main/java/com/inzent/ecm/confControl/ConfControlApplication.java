140452c6e45e19c0592ee0e3c71d90ee37df965c
nellizi <masterliz9095@gmail.com>
2023-11-17 15:25:30 +0900

    Feat : 프로젝트 생성

---
+++ src/main/java/com/inzent/ecm/confControl/ConfControlApplication.java
@@ -0,0 +1,13 @@
+package com.inzent.ecm.confControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inzent/ecm/confControl/ConfControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inzent.ecm.confControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
