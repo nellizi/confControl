c80a874aaf556a44e2c19a1f7042bca54d0e01e6
yoonhh <hdfull9820@gmail.com>
2023-12-20 14:06:08 +0900

    html 수정

---
+++ src/main/java/com/inzent/ecm/confControl/service/LocalAgentService.java
@@ -0,0 +1,29 @@
+package com.inzent.ecm.confControl.service;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import com.inzent.ecm.confControl.model.LocalAgentDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class LocalAgentService {
+
+	public void test() {
+		// TODO Auto-generated method stub
+		System.out.println("LocalAgent");
+	}
+
+	public LocalAgentDto getAttribute(Element ele) {
+
+		LocalAgentDto local = new LocalAgentDto();
+
+		local.setMachineId(ele.getAttribute("machineid"));
+
+		return local;
+
+	}
+
+}
